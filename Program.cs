a4115d4a930ec6107f62f0518f4b28d52320a5f9
jaquelineamara <jaquelineamara5@gmail.com>
2022-03-22 22:23:29 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿String frasesDigitada, fraseComDislalia;
+
+Console.Write("Digite uma flase: ");
+frasesDigitada = Console.ReadLine()!;
+
+fraseComDislalia = frasesDigitada
+.Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(fraseComDislalia);
+
